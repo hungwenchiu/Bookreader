3e25d38774800e671b18aebde940266d4291374a
nealzhu <zxykit@gmail.com>
2021-03-27 10:11:39 -0700

    init project

---
+++ src/main/java/edu/cmu/sda/bookreader/BookreaderApplication.java
@@ -0,0 +1,13 @@
+package edu.cmu.sda.bookreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreaderApplication.class, args);
+	}
+
+}
