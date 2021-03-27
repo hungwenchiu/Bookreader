71aeff260225002d0d2cd3bd64f6937e8f4a6250
nealzhu <zxykit@gmail.com>
2021-03-27 15:48:58 -0700

    💥 (react): added react front end

---
+++ app/src/Book.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+
+
+function Book() {
+    const [response, setResponse] = useState(null)
+
+    const getAllBooks = () => {
+        fetch("/api/books", {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then((res) => {
+                return res.json();
+            })
+            .then((result) => {
+                setResponse(result);
+            })
+    }
+
+    useEffect(() => {
+        getAllBooks()
+    }, [])
+
+    return (
+        <div className="Book">
+            All Books:
+            {
+                response && response.length > 0 && response.map((item) => <p>name: {item.name}; author: {item.author}; isbn: {item.isbn}</p>)
+            }
+        </div>
+    );
+}
+
+export default Book;
