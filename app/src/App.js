2b9f7499c29c3a6dc8b0cc18f0c2f44f4430b66b
Yunfan Hua <yunfanh@andrew.cmu.edu>
2021-04-02 16:43:49 -0700

    search functionality done

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import SearchPage from './pages/SearchPage'
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/SignUp" component={SignUp} />
+      <Route exact path="/SearchPage" component={SearchPage} />
     </Switch>
   );
 }
