82faed705f4a39731bdc25eb5c39441ca8805028
Shreya Chadha <shreyac@andrew.cmu.edu>
2021-04-20 16:32:15 -0700

    Update HomePage.js

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 const HomePage = () => (
   <Layout>
     <div>
+        // For reference to get user, needs to be removed
         current user is : {sessionStorage.getItem("currentUser")}
     </div>
   </Layout>
