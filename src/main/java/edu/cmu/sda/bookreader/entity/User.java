835680e3f70f032812106881a5f5387ec93b1275
shreya16aug <shreyac@andrew.cmu.edu>
2021-05-11 09:19:26 -0700

    Show bookshelves

--- src/main/java/edu/cmu/sda/bookreader/entity/User.java
+++ src/main/java/edu/cmu/sda/bookreader/entity/User.java
@@ -23,10 +23,39 @@ public class User {
     private String name;
     @NotNull
     private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Set<User> friends) {
+        this.friends = friends;
+    }
+
     @ManyToMany
     @JoinTable(
         name = "friendship",
         joinColumns = @JoinColumn(name = "user_id")
     )
     Set<User> friends = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
 }
