7f8530aa88abfa8ff6c938be3059a122b19e8248
shreya16aug <shreyac@andrew.cmu.edu>
2021-05-11 16:31:43 -0700

    Bookshelf UI

--- app/src/pages/BookShelvesPage.js
+++ app/src/pages/BookShelvesPage.js
@@ -6,6 +6,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Layout from '../components/Layout'
 import BookCard from '../components/BookCard'
+import List from '@material-ui/core/List';
+
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -72,17 +74,16 @@ export default function BookShelvesPage() {
   const [books, setBooks] = useState([])
   useEffect(() => {
     // get books from a bookshelf
-    const bookshelfName = "WantToRead"
+    const bookshelfName = "WantToRead";
     axios.get(`/api/bookshelves/${bookshelfName}/books?userID=${sessionStorage.getItem("currentUserID")}`)
     .then(res =>{
         setBooks(res.data);
-        console.log('Inside get books');
         console.log(res);
-        console.log("books "+books);
     })
     .catch( error => {
         console.log(error);
     });
+
   }, [])
 
   const handleChange = (event, newValue) => {
@@ -105,13 +106,19 @@ export default function BookShelvesPage() {
         <Tab label="Favorite" {...a11yProps(3)} />
       </Tabs>
       <TabPanel value={value} index={0}>
-        <BookCard image={altSrc} title='Book Title' author='Book Author' progress={50} />
+          <List>
+            {
+                books.map((book) => (
+                  <BookCard image = {book.volumeInfo?.imageLinks.thumbnail} title = {book.volumeInfo?.title} author = {book.volumeInfo?.authors ? book.volumeInfo?.authors[0] : "not available"} progress = {50}/>
+                ))
+            }
+          </List>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
       </TabPanel>
       <TabPanel value={value} index={2}>
-        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
       </TabPanel>
           
     </div>
